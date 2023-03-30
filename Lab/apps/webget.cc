32ec19122181eb7a8638986b01216e6bf4dc3f2e
cuizhu12138 <540643428@qq.com>
2023-03-30 12:39:38 +0800

    [lab2]Lab2Init

--- Lab/apps/webget.cc
+++ Lab/apps/webget.cc
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-void get_URL(const string &host, const string &path)
-{
+void get_URL(const string &host, const string &path) {
     // Your code here.
 
     // You will need to connect to the "http" service on
@@ -18,43 +17,20 @@ void get_URL(const string &host, const string &path)
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    // 新建一个socket
-    TCPSocket tcpsocket;
-
-    // 新建地址
-    Address address(host, "http");
-
-    // 链接目的地址
-    tcpsocket.connect(address);
-
-    // 发送数据包
-    tcpsocket.write("GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
-
-    // 接受消息
-    while (!tcpsocket.eof())
-    {
-        cout << tcpsocket.read();
-    }
-
-    tcpsocket.close();
     cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
     cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
-int main(int argc, char *argv[])
-{
-    try
-    {
-        if (argc <= 0)
-        {
-            abort(); // For sticklers: don't try to access argv[0] if argc <= 0.
+int main(int argc, char *argv[]) {
+    try {
+        if (argc <= 0) {
+            abort();  // For sticklers: don't try to access argv[0] if argc <= 0.
         }
 
         // The program takes two command-line arguments: the hostname and "path" part of the URL.
         // Print the usage message unless there are these two arguments (plus the program name
         // itself, so arg count = 3 in total).
-        if (argc != 3)
-        {
+        if (argc != 3) {
             cerr << "Usage: " << argv[0] << " HOST PATH\n";
             cerr << "\tExample: " << argv[0] << " stanford.edu /class/cs144\n";
             return EXIT_FAILURE;
@@ -66,9 +42,7 @@ int main(int argc, char *argv[])
 
         // Call the student-written function.
         get_URL(host, path);
-    }
-    catch (const exception &e)
-    {
+    } catch (const exception &e) {
         cerr << e.what() << "\n";
         return EXIT_FAILURE;
     }
